cd9282ce5203d91a36928f4c40b74ff4c0818cb6
Mike Mastromatteo <mike.mastrotrading13@gmail.com>
2020-07-15 00:18:07 -0400

    Adds PORT config for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const db = require("./db/db.json");
 const fs = require("fs");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
